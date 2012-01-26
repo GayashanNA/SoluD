fc81d5212126e86da89331d4e5e472e519e6d535
Gayashan Amarasinghe <chozrc@gmail.com>
2012-01-27 01:44:06 +0530

    text_input layout and view_solution layout are added. These layouts are used to insert equation and to view solution

--- SoluD/src/com/gayashanna/solud/SoluDStartActivity.java
+++ SoluD/src/com/gayashanna/solud/SoluDStartActivity.java
@@ -8,6 +8,6 @@ public class SoluDStartActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.text_input);
     }
 }
